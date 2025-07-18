a13a9979e797bbaa3f7b7b1bed1a4c9c8e6dd55c
Luis <luiscrsarmento@gmail.com>
2025-07-18 14:48:33 -0300

    database connection + test

---
+++ src/main/java/com/luis/Database.java
@@ -0,0 +1,40 @@
+package com.luis;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    
+    private static Database INSTANCE;
+    
+    private Connection connection;
+
+    public Database() {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+        } catch (SQLException e) {
+            System.err.println("Houve um problema ao criar o arquivo do banco!");
+        }
+    }
+    
+    public static Database getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Database();
+        }
+        
+        return INSTANCE;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+    
+    public void closeConnection() {
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            System.err.println("Houve um problema ao fechar a conexão do banco!");
+        }
+    }
+}
--- src/main/java/com/luis/Main.java
+++ src/main/java/com/luis/Main.java
@@ -1,7 +1,30 @@
 package com.luis;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+
+            Connection con = Database.getInstance().getConnection();
+            Statement statement = con.createStatement();
+            
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            statement.executeUpdate("drop table if exists person");
+            statement.executeUpdate("create table person (id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'leo')");
+            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person");
+            while(rs.next())
+            {
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace(System.err);
+        }
     }
 }
